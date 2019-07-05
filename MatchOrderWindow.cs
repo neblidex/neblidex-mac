6dc93cd1b05d6ed4d8344b7e8fc9b5dfd868dc85
david4neblio <43017430+david4neblio@users.noreply.github.com>
2019-07-04 23:50:49 -0400

    Merge version6 into master (#3)
    * Updated NBitcoin
    
    * Add files via upload
    
    * Updated to version 6
    
    * Updated readme

--- MatchOrderWindow.cs
+++ MatchOrderWindow.cs
@@ -147,26 +147,34 @@ namespace NebliDex_Linux
                 return;
             }
 
-            //Make sure that total is greater than block rates for both markets
+            //Make sure that total is greater than blockrate for the base market and the amount is greater than blockrate for trade market
             decimal block_fee1 = 0;
             decimal block_fee2 = 0;
-            if (App.MarketList[App.exchange_market].trade_wallet > 2 || App.MarketList[App.exchange_market].trade_wallet == 0)
+            int trade_wallet_blockchaintype = App.GetWalletBlockchainType(App.MarketList[App.exchange_market].trade_wallet);
+            int base_wallet_blockchaintype = App.GetWalletBlockchainType(App.MarketList[App.exchange_market].base_wallet);
+            block_fee1 = App.blockchain_fee[trade_wallet_blockchaintype];
+            block_fee2 = App.blockchain_fee[base_wallet_blockchaintype];
+
+            //Now calculate the totals for ethereum blockchain
+            if (trade_wallet_blockchaintype == 6)
             {
-                block_fee1 = App.blockchain_fee[0]; //Neblio fee
+                block_fee1 = App.GetEtherContractTradeFee();
             }
-            if (App.MarketList[App.exchange_market].base_wallet >= 0 && App.MarketList[App.exchange_market].base_wallet < 3)
+            if (base_wallet_blockchaintype == 6)
             {
-                block_fee2 = App.blockchain_fee[App.MarketList[App.exchange_market].base_wallet]; //Base fee
+                block_fee2 = App.GetEtherContractTradeFee();
             }
-            if (total < block_fee1 || total < block_fee2 || amount < block_fee1 || amount < block_fee2)
+
+            if (total < block_fee2 || amount < block_fee1)
             {
                 //The trade amount is too small
-				App.MessageBox(this, "Notice", "This trade amount is too small to match because it is lower than the blockchain fee.", "OK");
+                App.MessageBox(this, "Notice", "This trade amount is too small to match because it is lower than the blockchain fee.", "OK");
                 return;
             }
 
             //Because tokens are indivisible at the moment, amounts can only be in whole numbers
-            if (App.MarketList[App.exchange_market].trade_wallet > 2)
+            bool ntp1_wallet = App.IsWalletNTP1(App.MarketList[App.exchange_market].trade_wallet);
+            if (ntp1_wallet == true)
             {
                 if (Math.Abs(Math.Round(amount) - amount) > 0)
                 {
